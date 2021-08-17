d71dd5f367c5e0905e53350df2873b0912d34cef
jinuk-choi <81605147+jinuk-choi@users.noreply.github.com>
2021-08-17 11:43:56 +0900

    8.17 kakao login

--- src/main/java/com/project/example/controller/KakaoController.java
+++ src/main/java/com/project/example/controller/KakaoController.java
@@ -5,11 +5,8 @@ import java.util.HashMap;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +33,7 @@ public class KakaoController {
 	    }
 	    	return userInfo;
 	}
+	
+	
 	
 }
